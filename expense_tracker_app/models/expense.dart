6997d8c6c957fcb18f921545f8246f03da88af22
NikolaBWeb <nikolaberonja45@gmail.com>
2024-08-19 10:14:12 +0200

    Starting new course project Expense tracker app

---
+++ expense_tracker_app/models/expense.dart
@@ -0,0 +1,10 @@
+class Expense {
+  const Expense({required this.title,required this.amount,required date});
+
+  
+  final String title;
+  final double amount;
+  final String id;
+  final DateTime date;
+
+}
