b9dad16c7e570c3afa4465ded8716439ceb70cec
NikolaBWeb <nikolaberonja45@gmail.com>
2024-08-19 12:50:41 +0200

    Error fix

--- expense_tracker_app/lib/expenses.dart
+++ expense_tracker_app/lib/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +12,9 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title),
+  ]
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
