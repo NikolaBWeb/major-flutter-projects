239f7a5cceb846d14776900c2ef8053271565e41
NikolaBWeb <nikolaberonja45@gmail.com>
2024-08-23 15:21:54 +0200

    - Update to expense app. Added pie chart via fl_charts package.

---
+++ expense_tracker_app/lib/widgets/chart/chart_bar.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/main.dart';
+/* class ChartBar extends StatelessWidget {
+  const ChartBar({
+    super.key,
+    required this.fill,
+  });
+
+  final double fill;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: FractionallySizedBox(
+          heightFactor: fill,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(8)),
+              color: isDarkMode
+                  ? Theme.of(context).colorScheme.secondary
+                  : Theme.of(context).colorScheme.primary.withOpacity(0.65),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+} */
+
+PieChartSectionData buildPieChartSection({
+  required double value,
+  required Color color,
+  required String title,
+  double radius = 50,
+}) {
+  return PieChartSectionData(
+    value: value,
+    color: color,
+    title: title,
+    radius: radius,
+    titleStyle: const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    borderSide: const BorderSide(color: Colors.black, width: 2),
+  );
+}
+
+PieChartData getPieChartData(List<Expense> expenses) {
+  final foodExpenses =
+      ExpenseBucket.forCategory(expenses, Category.food).totalExpenses;
+  final leisureExpenses =
+      ExpenseBucket.forCategory(expenses, Category.leisure).totalExpenses;
+  final travelExpenses =
+      ExpenseBucket.forCategory(expenses, Category.travel).totalExpenses;
+  final workExpenses =
+      ExpenseBucket.forCategory(expenses, Category.work).totalExpenses;
+
+  return PieChartData(
+    sectionsSpace: 4, // Space between sections
+    centerSpaceRadius: 40, // Radius for the donut effect
+    sections: [
+      buildPieChartSection(
+        value: foodExpenses,
+        color:kColorScheme.onSecondaryContainer,
+        title: foodExpenses.toStringAsFixed(2),
+      ),
+      buildPieChartSection(
+        value: leisureExpenses,
+        color: kColorScheme.onSecondaryContainer,
+        title: leisureExpenses.toStringAsFixed(2),
+      ),
+      buildPieChartSection(
+        value: travelExpenses,
+        color:kColorScheme.onSecondaryContainer,
+        title: travelExpenses.toStringAsFixed(2),
+      ),
+      buildPieChartSection(
+        value: workExpenses,
+        color: kColorScheme.onPrimaryContainer,
+        title: workExpenses.toStringAsFixed(2),
+      ),
+    ],
+  );
+}
--- expense_tracker_app/lib/widgets/new_expense.dart
+++ expense_tracker_app/lib/widgets/new_expense.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({required this.onAddExpense, super.key});
+
+  final Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -11,17 +14,68 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _priceController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: now,
+      initialDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmout = double.tryParse(_priceController.text);
+    final amountIsinvalid = enteredAmout == null || enteredAmout <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsinvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure that valid title, amount, date and categorycwas entered'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(ctx);
+                },
+                child: const Text('Okay'))
+          ],
+        ),
+      );
+      return;
+    }
+    widget.onAddExpense(
+      Expense(
+          title: _titleController.text,
+          amount: enteredAmout,
+          date: _selectedDate!,
+          category: _selectedCategory),
+    );
+    Navigator.pop(context);
+  }
 
   @override
   void dispose() {
     _titleController.dispose();
+    _priceController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
@@ -31,28 +85,72 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text("Title"),
             ),
           ),
-          const  Row(
+          Row(
             children: [
               Expanded(
                 child: TextField(
+                  controller: _priceController,
+                  keyboardType: TextInputType.number,
                   maxLength: 10,
-                  decoration: InputDecoration(
-                    labelText: "Price",
+                  decoration: const InputDecoration(
+                    labelText: "Amount",
                     hintText: " Enter amount",
-                    prefixText: '\$',
+                    prefixText: '\$ ',
                   ),
                 ),
               ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "No date selected "
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value == null) {
+                        return;
+                      }
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text("Save Expense"),
-              )
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
             ],
           )
         ],
