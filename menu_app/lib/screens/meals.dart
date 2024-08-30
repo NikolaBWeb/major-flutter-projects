f9c980a6a0b1dbf093b8d500511f47ca4cc8e8e8
NikolaBWeb <nikolaberonja45@gmail.com>
2024-08-30 13:17:06 +0200

    Started meals_app.

---
+++ menu_app/lib/screens/meals.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:flutter/widgets.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key});
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- menu_app/test/widget_test.dart
+++ menu_app/test/widget_test.dart
@@ -8,13 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_internals/main.dart';
+import 'package:restaurant_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-   /*  await tester.pumpWidget(const MyApp());
- */
+    await tester.pumpWidget(const MyApp());
+
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
